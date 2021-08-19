872aa171683346a80a60460f027c3f4e0e25a7c0
nawhi <25394965+nawhi@users.noreply.github.com>
2021-08-19 17:22:35 +0100

    Move forward one position

--- src/mars_rover_clj/core.clj
+++ src/mars_rover_clj/core.clj
@@ -1,6 +1,12 @@
-(ns mars-rover-clj.core)
+(ns mars-rover-clj.core
+  (:require [clojure.string :as str]))
+
+(defn grid-to-string [grid] (str/join ":" (list (:x grid) (:y grid) (:face grid))))
 
 (defn rover-position
   "Given a grid and a list of moves, calculate the rover's final position"
   [grid moves]
-  "0:0:N")
+  (loop [position {:x 0 :y 0 :face "N"} moves moves]
+    (if (empty? moves)
+      (grid-to-string position)
+      (recur { :x 1 :y 0 :face "N"} (subs moves 1)))))
--- test/mars_rover_clj/core_test.clj
+++ test/mars_rover_clj/core_test.clj
@@ -5,8 +5,10 @@
 (def GRID-NO-OBSTACLES { :width 10 :height 10 :obstacles ()})
 
 (deftest a-test
-  (testing "Mars Rover"
-    (is (= (rover-position GRID-NO-OBSTACLES "") "0:0:N"))))
+  (testing "Rover starts on origin facing north"
+    (is (= "0:0:N" (rover-position GRID-NO-OBSTACLES ""))))
+  (testing "M moves rover forward one square"
+    (is (= "1:0:N" (rover-position GRID-NO-OBSTACLES "M")))))
 
 
 ; wraps around
