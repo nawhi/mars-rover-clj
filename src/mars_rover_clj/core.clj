b68b8d91f04c3919d3f37c0f1228e3ff7127a91a
nawhi <25394965+nawhi@users.noreply.github.com>
2021-08-19 17:56:49 +0100

    Rotate anticlockwise

--- src/mars_rover_clj/core.clj
+++ src/mars_rover_clj/core.clj
@@ -1,18 +1,25 @@
 (ns mars-rover-clj.core
   (:require [clojure.string :as str]))
 
-(defn grid-to-string [grid] (str/join ":" (list (:x grid) (:y grid) (:face grid))))
+(defn grid-to-string [grid] (str/join ":" (list (:x grid) (:y grid) (name (:face grid)))))
+
+(defn rotate-left [old-face]
+  (case old-face
+    :N :W
+    :W :S
+    :S :E
+    :E :N))
 
 (defn next-position [position move grid]
   (case move
     \M {:x (mod (inc (:x position)) (:height grid)) :y 0 :face "N"}
-    \L {:x (:x position) :y (:y position) :face "W"}
+    \L {:x (:x position) :y (:y position) :face (rotate-left (:face position))}
     ))
 
 (defn rover-position
   "Given a grid and a list of moves, calculate the rover's final position"
   [grid moves]
-  (loop [position {:x 0 :y 0 :face "N"} moves moves]
+  (loop [position {:x 0 :y 0 :face :N} moves moves]
     (if (empty? moves)
       (grid-to-string position)
       (let [move (first moves)
--- test/mars_rover_clj/core_test.clj
+++ test/mars_rover_clj/core_test.clj
@@ -12,7 +12,10 @@
     (is (= "2:0:N" (rover-position GRID-NO-OBSTACLES "MM")))
     (is (= "7:0:N" (rover-position GRID-NO-OBSTACLES "MMMMMMM"))))
   (testing "L rotates rover anticlockwise"
-    (is (= "0:0:W" (rover-position GRID-NO-OBSTACLES "L"))))
+    (is (= "0:0:W" (rover-position GRID-NO-OBSTACLES "L")))
+    (is (= "0:0:S" (rover-position GRID-NO-OBSTACLES "LL")))
+    (is (= "0:0:E" (rover-position GRID-NO-OBSTACLES "LLL")))
+    (is (= "0:0:N" (rover-position GRID-NO-OBSTACLES "LLLL"))))
   (testing "Rover wraps round at the end of the grid"
     (is (= "1:0:N" (rover-position GRID-NO-OBSTACLES "MMMMMMMMMMM")))))
 
