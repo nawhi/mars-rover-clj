6bc4575abb939496cbdafe13945bef715dd9f209
nawhi <25394965+nawhi@users.noreply.github.com>
2021-08-22 20:49:09 +0100

    Make all except main function non-public

--- src/mars_rover_clj/core.clj
+++ src/mars_rover_clj/core.clj
@@ -1,29 +1,29 @@
 (ns mars-rover-clj.core
   (:require [clojure.string :as str]))
 
-(defn grid-to-string [grid] (str/join ":" (list (:x grid) (:y grid) (name (:face grid)))))
+(defn- grid-to-string [grid] (str/join ":" (list (:x grid) (:y grid) (name (:face grid)))))
 
-(defn rotate-left [old-face]
+(defn- rotate-left [old-face]
   (case old-face
     :N :W
     :W :S
     :S :E
     :E :N))
 
-(defn rotate-right [old-face]
+(defn- rotate-right [old-face]
   (case old-face
     :N :E
     :E :S
     :S :W
     :W :N))
 
-(defn dimension-of [face]
+(defn- dimension-of [face]
   (case face (:N :S) :y (:E :W) :x))
 
-(defn update-dimension-of [face]
+(defn- update-dimension-of [face]
   (case face (:N :E) inc (:S :W) dec))
 
-(defn next-position [position move grid]
+(defn- next-position [position move grid]
   (case move
     \M (let [dimension (dimension-of (:face position))
              update-dimension (update-dimension-of (:face position))]
@@ -31,7 +31,7 @@
     \L (update position :face rotate-left)
     \R (update position :face rotate-right)))
 
-(defn coords [position] (select-keys position [:x :y]))
+(defn- coords [position] (select-keys position [:x :y]))
 
 (defn rover-position
   "Given a grid and a list of moves, calculate the rover's final position"
